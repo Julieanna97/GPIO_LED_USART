5355a57d6b0bdfb45302e1d8152aff70bcce0880
Julieanna97 <kisamae1997@gmail.com>
2023-06-14 10:01:52 +0200

    Added functionality in my_stm32f4_gpio_driver.h file

--- Development folder/Drivers/my_stm32f4_gpio_driver.h
+++ Development folder/Drivers/my_stm32f4_gpio_driver.h
@@ -4,5 +4,18 @@
 #include <stdint.h>
 
 #define PERIPH_BASE 0x40000000U // Base address for peripheral work
+#define AHB1PERIPH_BASE (PERIPH_BASE + 0x10U) // An offset from the origin leading to the peripheral register AHB1
+#define RCC_BASE (PERIPH_BASE + 0x30U) // An offset from the origin leading to the clock-register for the GPIO
+
+#define _IO volatile
+
+typedef struct
+{
+    _IO uint32_t MODER;
+    _IO uint32_t OTYPER;
+    _IO uint32_t OSPEEDR;
+
+}GPIO_Typedef;
+
 
 #endif
