61a528bb4ded7a8fabb6c5da3255137c279fbb9b
Julieanna97 <kisamae1997@gmail.com>
2023-06-19 19:43:08 +0200

    added functionalities to my_stm32f4_gpio_lib.h

---
+++ Development folder/Library/my_stm32f4_gpio_lib.c
@@ -0,0 +1,10 @@
+#include "my_stm32f4_gpio_lib.h"
+
+
+int main(void)
+{
+
+
+
+    return 0;
+}
--- Development folder/Library/my_stm32f4_gpio_lib.h
+++ Development folder/Library/my_stm32f4_gpio_lib.h
@@ -3,4 +3,80 @@
 
 #include "my_stm32f4_gpio_driver.h"
 
+/**
+ * @brief Defines constants related to GPIO pins
+ * and their corresponding modes and pull configurations.
+ */
+
+#define GPIO_PIN_0 ((uint16_t) 0x0001)
+#define GPIO_PIN_1 ((uint16_t) 0x0002)
+#define GPIO_PIN_2 ((uint16_t) 0x0004)
+#define GPIO_PIN_3 ((uint16_t) 0x0008)
+#define GPIO_PIN_4 ((uint16_t) 0x0010)
+#define GPIO_PIN_5 ((uint16_t) 0x0020)
+#define GPIO_PIN_6 ((uint16_t) 0x0040)
+#define GPIO_PIN_7 ((uint16_t) 0x0080)
+#define GPIO_PIN_8 ((uint16_t) 0x0100)
+#define GPIO_PIN_9 ((uint16_t) 0x0200)
+#define GPIO_PIN_10 ((uint16_t) 0x0400)
+#define GPIO_PIN_11 ((uint16_t) 0x0800)
+#define GPIO_PIN_12 ((uint16_t) 0x1000)
+#define GPIO_PIN_13 ((uint16_t) 0x2000)
+#define GPIO_PIN_14 ((uint16_t) 0x4000)
+#define GPIO_PIN_15 ((uint16_t) 0x8000)
+#define GPIO_PIN_ALL ((uint16_t) 0xFFFF)
+
+
+#define GPIO_MODE_INPUT       0x00U
+#define GPIO_MODE_OUTPUT_PP   0x11U
+#define GPIO_MODE_OUTPUT_OD   0x01U
+#define GPIO_MODE_AF_PP       0x02U
+#define GPIO_MODE_AF_OD       0x12U
+
+#define GPIO_NOPULL           0x00U
+#define GPIO_PULLUP           0x01U
+#define GPIO_PULLDOWN         0x02U
+
+/**
+ * @brief Configuration parameters for initializing
+ * GPIO pin.
+ */
+typedef struct
+{
+    uint32_t Pin;
+    uint32_t Mode;
+    uint32_t Pull;
+    uint32_t Speed;
+    uint32_t Alternate;
+
+}GPIO_InitTypeDef;
+
+/**
+ * @brief States of the GPIO pin.
+ * 
+ */
+typedef enum
+{
+    GPIO_PIN_RESET = 0,
+    GPIO_PIN_SET = 1
+
+}GPIO_PinState;
+
+/* Initalization of GPIO modules*/
+void LIB_GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_InitTypeDef *GPIO_Init);
+GPIO_PinState LIB_GPIO_readPinState(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
+void LIB_GPIO_writePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
+void LIB_GPIO_togglePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
+
+/**
+ * @brief Enable clock signals for the corresponding
+ * GPIO ports: A, B, C, D, E, H
+ */
+void __LIB_RCC_GPIOA_CLK_ENABLE(void);
+void __LIB_RCC_GPIOB_CLK_ENABLE(void);
+void __LIB_RCC_GPIOC_CLK_ENABLE(void);
+void __LIB_RCC_GPIOD_CLK_ENABLE(void);
+void __LIB_RCC_GPIOE_CLK_ENABLE(void);
+void __LIB_RCC_GPIOH_CLK_ENABLE(void);
+
 #endif
