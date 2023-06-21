610b915159c3a50cfd306fd9075a059c0009b971
Julieanna97 <kisamae1997@gmail.com>
2023-06-21 14:58:09 +0200

    Added USART & its' registers in header file

--- Development folder/Drivers/my_stm32f4_gpio_driver.h
+++ Development folder/Drivers/my_stm32f4_gpio_driver.h
@@ -8,8 +8,15 @@
  * with the development card 
  */
 #define PERIPH_BASE 0x40000000U // Base address for peripheral work
-#define AHB1PERIPH_BASE (PERIPH_BASE + 0x20000U) // An offset from the origin leading to the peripheral register AHB1
+#define AHB1PERIPH_BASE 0x40020000U // Base address for peripheral register AHB1
+#define APB1PERIPH_BASE PERIPH_BASE // Base address for peripheral register APB1
+#define APB2PERIPH_BASE 0x40010000U // Base address for peripheral register APB2
+
+
 #define RCC_BASE (AHB1PERIPH_BASE + 0x3800U) // An offset from the origin leading to the clock-register for the GPIO
+#define USART1_BASE (APB2PERIPH_BASE + 0x1000U) // An offset from the APB2 origin leading to USART1
+#define USART2_BASE (APB1PERIPH_BASE + 0x4400U) // An offset from the APB1 origin leading to USART2
+#define USART6_BASE (APB2PERIPH_BASE + 0x1400U) // An offset from the APB2 origin leading to USART6
 
 /**
  * @brief Defines memory addresses for different
@@ -30,55 +37,68 @@
  */
 typedef struct
 {
-    /* General purpose I/O reference registers */
-    __IO uint32_t MODER;    /* Mode register */
-    __IO uint32_t OTYPER;   /* Output type register */
-    __IO uint32_t OSPEEDR;  /* Output speed register */
-    __IO uint32_t PUPDR;    /* Pull-up/pull-down register */
-    __IO uint32_t IDR;      /* Input data register */
-    __IO uint32_t ODR;      /* Output data register */
-    __IO uint32_t BSRR;     /* Bit set/reset register */
-    __IO uint32_t LCKR;     /* Configuration lock register */
-    __IO uint32_t AFR[2];   /* Alternate function low/high register */
+        /* General purpose I/O reference registers */
+        __IO uint32_t MODER;    /* Mode register */
+        __IO uint32_t OTYPER;   /* Output type register */
+        __IO uint32_t OSPEEDR;  /* Output speed register */
+        __IO uint32_t PUPDR;    /* Pull-up/pull-down register */
+        __IO uint32_t IDR;      /* Input data register */
+        __IO uint32_t ODR;      /* Output data register */
+        __IO uint32_t BSRR;     /* Bit set/reset register */
+        __IO uint32_t LCKR;     /* Configuration lock register */
+        __IO uint32_t AFR[2];   /* Alternate function low/high register */
 
 }GPIO_TypeDef;
 
 typedef struct
 {
-    /* Reset and clock control reference registers */
-    __IO uint32_t CR;
-    __IO uint32_t PLLCFGR;
-    __IO uint32_t CFGR;
-    __IO uint32_t CIR;
-    __IO uint32_t AHB1RSTR;
-    __IO uint32_t AHB2RSTR;
-            uint32_t DUMMY1;
-            uint32_t DUMMY2;
-    __IO uint32_t APB1RSTR;
-    __IO uint32_t APB2RSTR;
-            uint32_t DUMMY3[2];
-    __IO uint32_t AHB1ENR;
-    __IO uint32_t AHB2ENR;
-            uint32_t DUMMY4[2];
-    __IO uint32_t APB1ENR;
-    __IO uint32_t APB2ENR;
-            uint32_t DUMMY5[2];
-    __IO uint32_t AHB1LPENR;
-    __IO uint32_t AHB2LPENR;
-            uint32_t DUMMY6[2];
-    __IO uint32_t APB1LPENR;
-    __IO uint32_t APB2LPENR;
-            uint32_t DUMMY7[2];
-    __IO uint32_t BDCR;
-    __IO uint32_t CSR;
-            uint32_t DUMMY8[2];
-    __IO uint32_t SSCGR;
-    __IO uint32_t PLLI2SCFGR;
-            uint32_t DUMMY9;
-    __IO uint32_t DCKCFGR;
+        /* Reset and clock control reference registers */
+        __IO uint32_t CR;           /* Clock control register */
+        __IO uint32_t PLLCFGR;      /* PPL configuration register */
+        __IO uint32_t CFGR;         /* Clock configuration register */
+        __IO uint32_t CIR;          /* Clock interrupt register */
+        __IO uint32_t AHB1RSTR;     /* AHB1 peripheral reset register */
+        __IO uint32_t AHB2RSTR;   
+                uint32_t DUMMY1;    /* fill in variable for configuration bits */
+                uint32_t DUMMY2;
+        __IO uint32_t APB1RSTR;     /* APB1 peripheral reset register */
+        __IO uint32_t APB2RSTR;     
+                uint32_t DUMMY3[2];
+        __IO uint32_t AHB1ENR;      /* AHB1 peripheral clock enable register */
+        __IO uint32_t AHB2ENR;      
+                uint32_t DUMMY4[2];
+        __IO uint32_t APB1ENR;
+        __IO uint32_t APB2ENR;
+                uint32_t DUMMY5[2];
+        __IO uint32_t AHB1LPENR;    /* AHB1 peripheral clock enable in low power mode register */
+        __IO uint32_t AHB2LPENR;
+                uint32_t DUMMY6[2];
+        __IO uint32_t APB1LPENR;
+        __IO uint32_t APB2LPENR;
+                uint32_t DUMMY7[2];
+        __IO uint32_t BDCR;         /* Backup domain control register */
+        __IO uint32_t CSR;          /* Clock control & status register */
+                uint32_t DUMMY8[2];
+        __IO uint32_t SSCGR;        /* Spread spectrum clock generation register */
+        __IO uint32_t PLLI2SCFGR;   /* PLLI2S configuration register */
+                uint32_t DUMMY9;
+        __IO uint32_t DCKCFGR;      /* Dedicated clocks configuration register */
 
 
 }RCC_TypeDef;
+
+typedef struct
+{
+        __IO uint32_t SR;       /* Status register */
+        __IO uint32_t DR;       /* Data register */
+        __IO uint32_t BRR;      /* Baud rate register */
+        __IO uint32_t CR1;      /* Control register */
+        __IO uint32_t CR2;
+        __IO uint32_t CR3;
+        __IO uint32_t GTPR;     /* Guard time and prescaler register */
+
+}USART_RegTypeDef;
+
 
 #define RCC ((RCC_TypeDef *)RCC_BASE)
 
@@ -98,5 +118,10 @@ typedef struct
 
 #define MODER_5_OUT 1<<10
 #define LED_PIN 1<<5
+
+#define USART1 ((USART_RegTypeDef *)USART1_BASE);
+#define USART2 ((USART_RegTypeDef *)USART2_BASE);
+#define USART6 ((USART_RegTypeDef *)USART6_BASE);
+
 
 #endif
